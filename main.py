5e1453fb0ea036cafbeac05489dfa92693c45b0b
DanielJAD <daniel.derrick71+1@gmail.com>
2022-08-31 20:53:51 +0100

    Added generating profits.

--- main.py
+++ main.py
@@ -13,11 +13,15 @@ class Employee:
     def fire(self):
         self.status = 0
 
+    def gen_profits(self, time):
+        return (self.expertise * self.happiness**2 * self.ethic)*(time**0.5)
+
 
 class CompanyStats:
-    def __init__(self, funds = 0):
-        self.funds = 1000
+    def __init__(self, funds=0, time = 0):
+        self.funds = (1000 if funds == 0 else funds)
         self.employees = []
+        self.time = time
 
     def hire(self, person, not_hired_list):
         if person not in self.employees and person in not_hired_list:
@@ -31,7 +35,22 @@ class CompanyStats:
             not_hired_list.append((person))
             person.fire()
 
+    def advance_time(self, days):
+        self.time += days
 
+    def profit_report(self):
+        for person in self.employees:
+            print(person.name + ' generates ' + str(person.gen_profits(1)) + ' per day.')
+
+    def gen_company_profits(self, time):
+        print('\nGenerating profits for ' + str(time) + ' days...\n')
+        total = 0
+        for employee in self.employees:
+            total += employee.gen_profits(time)
+        total = round(total*100) / 100
+        print('Profit made: £' + str(total) + '.')
+        self.funds += total
+        print('New company funds: £' + str(self.funds) + '!')
 
 def readnames(list):
     if len(list) == 0:
@@ -51,23 +70,22 @@ def employeeReport(company, not_hired_list):
     print(' ___________________________________ ')
 
 
+# Init stuff
 employee1 = Employee('David Miles', 35, 9, 5, 8, 0)
 employee2 = Employee('Jack White', 25, 7, 4, 2, 0)
-
 not_hired_list = [employee1, employee2]
 
 
-# def hire_employee(person):
-
-
 def startUp():
-
     not_hired_list = [employee1, employee2]
     this_company = CompanyStats()
     this_company.hire(employee1, not_hired_list)
     this_company.hire(employee2, not_hired_list)
 
     employeeReport(this_company, not_hired_list)
+
+    this_company.gen_company_profits(10)
+    this_company.profit_report()
 
     return 0
 
